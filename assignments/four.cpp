0e16be14839e9cd1c6c43a8e1a2504492eeeee0e
nforoutann <nazaninforoutan6@gmail.com>
2025-09-19 15:32:06 +0330

    feat: cpp version of four added

---
+++ assignments/four.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void fun(string str, int num){
+    int cpy = num-1;
+
+    if(str == ""){
+        while(cpy != 0){
+            fun(to_string(num-cpy), cpy);
+            cpy--;
+        }
+    } else{
+        if (num == 0) return;
+        cout << str << "-" << num << endl;
+        while(cpy != 0){
+            fun(str+"-"+to_string(num-cpy), cpy);
+            cpy--;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    fun("", n);
+}
