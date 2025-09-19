4b086791fd17598da9020a7b9a21dedd2d18c0ae
nforoutann <nazaninforoutan6@gmail.com>
2025-09-20 01:42:31 +0330

    feat: no.4 not working ahhhh

---
+++ assignments/four.s
@@ -0,0 +1,104 @@
+.data
+dash: .asciz "-"
+
+.text
+.global _start
+_start:
+	
+	#get n
+	li a7, 5
+	ecall
+	mv s0, a0
+	
+	#a1 as the second argument that the define the depth of stack
+	add a1, x0, x0
+	
+	#call fun
+	jal ra, fun
+	
+	#over
+	done:
+		j done	
+	
+	fun:
+		addi a2, a0, -1
+		beq a1, x0, if_empty_stack
+		bne a1, x0, if_not_empty_stack
+		
+		if_empty_stack:
+			loop1:
+				beq a2, x0, end_loop
+				mv a0, a2
+				addi a1, a1, 1
+				addi sp, sp, -8
+				sw ra, 4(sp)
+				sw a0, 0(sp)
+				jal ra, fun
+				#???
+				lw ra, 4(sp)
+				addi a2, a2, -1
+				j loop1
+
+		if_not_empty_stack:
+			#a0 is num, a1 is stack depth, a2 is cpy or num-1
+			beq a0, x0, end_loop
+			
+			#print prefix
+			#t0 is dynamic depth to iterate
+			add t0, a1, x0
+			print:
+				beq t0, x0, end_print
+				addi t1, x0, 8
+				add t4, a1, -1
+				mul t1, t1, t4
+				add t2, sp, t1
+				lw t3, 0(t2)
+				mv a3, a0
+
+				mv a0, t3
+				li a7, 1
+				ecall
+				la a0, dash
+				li a7, 4
+				ecall
+
+				mv a0, a3
+				addi t0, t0, -1
+				j print
+
+			end_print:
+				#print newline
+				mv a3, a0
+				mv a0, a2
+				li a7, 1
+				ecall
+				li a0, 10
+				li a7, 11
+				ecall
+				mv a0, a3
+				
+			loop2:
+				beq a2, x0, end_loop
+				sub t0, a0, a2
+				
+				addi a1, a1, 1
+				addi sp, sp, -8
+				sw t0, 0(sp)
+				sw ra, 4(sp)
+				
+				mv a3, a0
+				mv a0, a2
+				jal ra, fun
+				#???
+				lw ra, 4(sp)
+				mv a0, a3
+				addi a2, a2, -1
+				j loop2
+					
+					
+		end_loop:
+			ret
+				
+
+		return:
+			ret
